02bd0654c2fcac4d9593f917b5017b1f777b3eb1
Bruno Reis <85531625+brunocbreis@users.noreply.github.com>
2023-06-08 12:36:01 -0300

    add settings and defaults

--- PosterizeTime.lua
+++ PosterizeTime.lua
@@ -1,19 +1,23 @@
 FuRegisterClass("PosterizeTime", CT_Tool, {
-    REGS_Name = "PosterizeTime", 
+    REGS_Name = "PosterizeTime",
     REGS_Category = "Fuses"
 })
 
 
 function Create()
-    InFPS = self:AddInput("FPS", "FPS", {
+    InFPS = self:AddInput("Frame Rate", "FPS", {
         LINKID_DataType = "Number",
         INPID_InputControl = "SliderControl",
         INP_MaxScale = 24,
-        INP_MinScale = .1,
-        INP_Default = 12,
-        INP_External = false, -- not animatable
+        INP_MinScale = 1,
+        INP_MaxAllowed = 24,
+        INP_MinAllowed = 0.1,
+        IC_DisplayedPrecision = 1,
+        -- INP_Default = 12,
+        INP_DelayDefault = true,
+        INPS_StatusText = "Frame rate to simulate"
     })
-    
+
     InImage = self:AddInput("Input", "Input", {
         LINKID_DataType = "Image",
         LINK_Main = 1,
@@ -26,21 +30,30 @@ function Create()
 end
 
 
-
-
 function OnAddToFlow()
-    currentFPS = self.Comp:GetPrefs().Comp.FrameFormat.Rate
+    CurrentFPS = self.Comp:GetPrefs().Comp.FrameFormat.Rate
+    InFPS:SetAttrs({
+        INP_Default = CurrentFPS/2,
+        INP_MaxScale = CurrentFPS,
+        INP_MaxAllowed = CurrentFPS,
+    })
 end
 
 
 function Posterize(time, reqFPS)
-    local fpsRatio = currentFPS / reqFPS
+    local fpsRatio = CurrentFPS / reqFPS
     return math.floor(time / fpsRatio) * fpsRatio
 end
 
 
 function Process(req)
     local reqFPS = InFPS:GetValue(req).Value
+
+    if reqFPS == CurrentFPS then
+        OutImage:Set(req, InImage:GetValue(req))
+        return
+    end
+
     local posterizedTime = Posterize(req.Time, reqFPS)
 
     local img = InImage:GetSource(posterizedTime)
